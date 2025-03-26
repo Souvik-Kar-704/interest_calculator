a28dbbe96b40f7a15a8f394acda750dbbd5a8115
Souvik Kar <souvikkrkar07@gmail.com>
2025-03-26 15:31:25 +0530

    version 1.0.0
    starting

---
+++ api.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware #Import the CORS middleware
+
+import requests
+
+app = FastAPI()
+
+# Add the CORS middleware to your application
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows requests from your HTML file
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
+
+@app.get("/")
+def read_root():
+    return {"message": "hello world"}
+
+@app.get("/simple/{amount}/{rate}/{time}")
+def calc(amount: float, rate:float, time: float):
+    return amount*rate*time
